4f150b49ffd60083022268338f5b72e7036d6139
fixedalphabet <john.hamilton248@gmail.com>
2019-10-01 17:20:56 -0500

    Added models to admin

--- writingsite/frontpage/admin.py
+++ writingsite/frontpage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Tags
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal = ['tags', ]
+
+    list_display = ('title', 'date_uploaded')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tags)
--- writingsite/frontpage/models.py
+++ writingsite/frontpage/models.py
@@ -6,10 +6,19 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
-    file_upload = models.FileField()
+    file_upload = models.FileField(blank=True, null=True)
     date_uploaded = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tags', blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Tags(models.Model):
     tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        verbose_name_plural = 'Tags'
