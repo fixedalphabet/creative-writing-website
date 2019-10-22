25383be83ee709fad41966cd20ee1a428dcb3ed0
fixedalphabet <john.hamilton248@gmail.com>
2019-10-22 15:26:38 -0500

    created index page for submissions

--- writingsite/submissions/models.py
+++ writingsite/submissions/models.py
@@ -6,8 +6,10 @@ from django.db import models
 class Writing(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    date_uploaded = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
     content = models.TextField()
     writing = models.ForeignKey(Writing, on_delete=models.CASCADE)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
--- writingsite/submissions/views.py
+++ writingsite/submissions/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Welcome to the users page, this is where profiles will eventually be.")
+    writingList = Writing.objects.order_by('date_uploaded').reverse()
+    data = {'writingList': writingList}
+    return render(request, 'submissions/index.html')
