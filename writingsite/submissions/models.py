ac111092330fb5872c08782b9cee8cc1b3d358e0
fixedalphabet <john.hamilton248@gmail.com>
2019-10-25 18:19:31 -0500

    added view submission page

--- writingsite/submissions/models.py
+++ writingsite/submissions/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
 
 
 class Writing(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     date_uploaded = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
 
 
 class Comment(models.Model):
     content = models.TextField()
     writing = models.ForeignKey(Writing, on_delete=models.CASCADE)
     date_uploaded = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
--- writingsite/submissions/urls.py
+++ writingsite/submissions/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('view/<int:submission_id>', views.view, name='view'),
 ]
