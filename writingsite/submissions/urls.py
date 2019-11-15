fba20d2d51c7dc2aead8849b043f31e48d513de7
fixedalphabet <john.hamilton248@gmail.com>
2019-11-15 16:09:50 -0600

    added comments to submissions view

--- writingsite/submissions/urls.py
+++ writingsite/submissions/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('view/<int:submission_id>', views.view, name='view'),
+    path('', views.index, name='submissions/index'),
+    path('view/<int:submission_id>', views.view, name='submissions/view'),
+    path('<int:submission_id>/comment', views.comment, name='submissions/comment'),
 ]
--- writingsite/submissions/views.py
+++ writingsite/submissions/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 
 # Create your views here.
 
 
 def index(request):
+
     writingList = Writing.objects.all()
     data = {'writingList': writingList}
     return render(request, 'submissions/index.html', data)
@@ -13,4 +14,16 @@ def index(request):
 
 def view(request, submission_id):
     submission = get_object_or_404(Writing, pk=submission_id)
-    return render(request, 'submissions/view.html', {'submission':submission})
+    comments = Comment.objects.order_by('date_uploaded').reverse()
+    return render(request, 'submissions/view.html', {'submission': submission, 'comments': comments})
+
+
+def comment(request, submission_id):
+    submission = get_object_or_404(Writing, pk=submission_id)
+    text = request.POST['text']
+    if text == '':
+        return redirect(view)
+
+    Comment.objects.create(content=text, writing=submission, author=request.user)
+
+    return redirect(view, submission_id)
